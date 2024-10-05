b20b49e1b894fef9583c60c73ceedaf039d85d58
ramday <f223654@cfd.nu.edu.pk>
2024-10-05 02:30:33 -0700

    added comments

--- git_demo/src/git_demo/Library.java
+++ git_demo/src/git_demo/Library.java
@@ -1,9 +1,27 @@
-package git_demo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Library {
+    private List<String> books = new ArrayList<>();
 
-	public Library() {
-		// TODO Auto-generated constructor stub
-	}
+    public void addBook(String bookName) {
+        books.add(bookName);
+        System.out.println(bookName + " added to library.");
+    }
 
+    public void listBooks() {
+        System.out.println("Books in library: " + books);
+    }
+    
+    public boolean searchBook(String bookName) {
+    	return books.contains(bookName);
+    	}
+    //checking
+
+    public static void main(String[] args) {
+        Library myLibrary = new Library();
+        myLibrary.addBook("1984 by George Orwell");
+        myLibrary.addBook("To Kill a Mockingbird by Harper Lee");
+        myLibrary.listBooks();
+    }
 }
